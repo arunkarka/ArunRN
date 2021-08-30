0f69f9b13f35d8958bd284442029b5f6afc71d4f
ringpisolve <ringpisolve@gmail.com>
2021-08-29 21:51:10 -0500

    Added UI and eslint

--- App.js
+++ App.js
@@ -1,16 +1,50 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+import React, { Fragment, Component } from 'react';
+import { SafeAreaView, StyleSheet, View, Button, Text } from 'react-native';
+
+class App extends Component {
+  state = { count: 0 };
+  decrementCount() {
+    let { count } = this.state;
+    count--;
+    this.setState({
+      count,
+    });
+  }
+  incrementCount() {
+    let { count } = this.state;
+    count++;
+    this.setState({
+      count,
+    });
+  }
+  render() {
+    const { count } = this.state;
+    return (
+      <View style={styles.container}>
+        <Button title="increment" onPress={() => this.incrementCount()} />
+        <Text>{count}</Text>
+        <Button title="decrement" onPress={() => this.decrementCount()} />
+      </View>
+    );
+  }
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    marginTop: 40,
+  },
+});
+
+export default App;
